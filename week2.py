fe4601ccfac21c3fb65d288eb9849b18fe894f9f
Kasidit0052 <60087310+Kasidit0052@users.noreply.github.com>
2020-02-01 08:35:38 +0700

    'change'

---
+++ week2.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 31 11:51:45 2020
+
+@author: User
+"""
+
+import re
+pin='4324'
+
+def validate_pin(pin):
+    pat = re.compile("^[0-9]{4}([0-9]{2})?$")
+    if pat.match(pin):
+        return True
+    return False
+print(validate_pin(pin))
